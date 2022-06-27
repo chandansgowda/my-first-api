b245378dd80d2a5be916ea58b6c69a89b3eb112a
chandsgowda <chandansuresh007@gmail.com>
2022-06-27 21:07:14 +0530

    path parameters

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import imp
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def index():
     return "Hello! This is my first API"
+
+@app.get("/blogs/unpublished")
+def unpublished():
+    return {'data': 'unpublished blogs'}
+
+@app.get("/blogs/{id}")
+def about(id: int):
+    #fetch blog with id 
+    return {'data':id}
