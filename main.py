648e23fc901c4670fb4ec6580084ce676023de70
chandsgowda <chandansuresh007@gmail.com>
2022-06-27 21:21:31 +0530

    query parameters

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,6 +6,14 @@ app = FastAPI()
 @app.get("/")
 def index():
     return "Hello! This is my first API"
+
+@app.get("/blogs/")
+def blogs(limit: int = 10, published: bool = True, sort: Optional[str] = "latest"):
+    if published:
+        return {"data": f"{limit} published blogs"}
+    else:
+        return {"data": f"{limit} unpublished blogs"}
+
 
 @app.get("/blogs/unpublished")
 def unpublished():
